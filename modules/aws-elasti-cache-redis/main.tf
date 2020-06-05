0c9d24b4e327fb958feb6d89ced093557e108d8a
kush Kumar <kush.kumar@ssh.com>
2020-06-05 11:43:28 +0100

    removed parameter_group_name, as its using default value

--- modules/aws-elasti-cache-redis/main.tf
+++ modules/aws-elasti-cache-redis/main.tf
@@ -3,7 +3,6 @@ resource "aws_elasticache_cluster" "default" {
   engine               = "redis"
   node_type            = var.node_type
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis5.0"
   engine_version       = "5.0.6"
   port                 = 6379
   security_group_ids   = var.security_group_ids
